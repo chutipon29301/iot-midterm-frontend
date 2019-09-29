80e4a66889f8dd876891f427bd19b08ca9d14d36
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-09-29 21:58:36 +0700

    :sparkles: Add sensor storage

--- src/store.ts
+++ src/store.ts
@@ -12,18 +12,26 @@ export enum TrafficLightColor {
 export default new Vuex.Store({
   state: {
     trafficLights: [] as TrafficLightColor[],
+    irSensorsState: [] as boolean[],
   },
   getters: {
     trafficLights: (state) => state.trafficLights,
+    irSensorState: (state) => state.irSensorsState,
   },
   mutations: {
     updateTrafficLight(state, payload: TrafficLightColor[]) {
       state.trafficLights = payload;
     },
+    updateIRSensorState(state, payload: boolean[]) {
+      state.irSensorsState = payload;
+    }
   },
   actions: {
     SOCKET_onTrafficLightChange({ commit }, message: TrafficLightColor[]) {
       commit('updateTrafficLight', message);
+    },
+    SOCKET_onIRSensorChange({ commit }, message: boolean[]) {
+      commit('updateIRSensorState', message);
     },
   },
 });
